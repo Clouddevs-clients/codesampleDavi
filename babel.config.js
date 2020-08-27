5535b0c00f3264ed3b94b1356aaca5b69511eec9
alclouddevs <64176077+alclouddevs@users.noreply.github.com>
2020-08-27 21:36:15 +0100

    Add files via upload

---
+++ babel.config.js
@@ -0,0 +1,40 @@
+module.exports = (api) => {
+  api.cache(true);
+
+  return {
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          useBuiltIns: 'usage',
+          corejs: 3,
+          targets: {
+            browsers: 'Last 2 Chrome versions, Firefox ESR',
+            node: 'current',
+          },
+        },
+      ],
+      [
+        '@babel/preset-react',
+        {
+          development: process.env.BABEL_ENV !== 'build',
+        },
+      ],
+      '@babel/preset-typescript',
+    ],
+    plugins: ['transform-class-properties'],
+    env: {
+      build: {
+        ignore: [
+          '**/*.test.tsx',
+          '**/*.test.ts',
+          '**/*.story.tsx',
+          '__snapshots__',
+          '__tests__',
+          '__stories__',
+        ],
+      },
+    },
+    ignore: ['node_modules'],
+  };
+};
